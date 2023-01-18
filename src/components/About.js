fb9b9ab84e78a7fc9aeeaddb45dee9649f2a7f2b
Prashant Mishra <info.prashant08@gmail.com>
2023-01-18 03:39:03 -0800

    images changes fixed

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center" style={{paddingBottom:'0px'}}>
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center" >
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Prashant Mishra.
             <br className="hidden lg:inline-block" />I love to build amazing
@@ -32,7 +32,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./myimgfst.jpg"
+            src="./myimgsec.jpg"
           />
         </div>
       </div>
